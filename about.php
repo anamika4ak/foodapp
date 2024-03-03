d7c647fa1e1523c93eaaf3a2623ee540496dffcf
Anamika Kumari <anamika170304@gmail.com>
2024-03-03 21:47:45 +0530

    Initial commit

---
+++ about.php
@@ -0,0 +1,24 @@
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                <hr class="divider my-4" />
+            </div>
+
+        </div>
+    </div>
+</header>
+
+<section class="page-section">
+    <div class="container">
+        <!-- <?php echo html_entity_decode($_SESSION['setting_about_content']) ?> -->
+        Welcome to AK-Food Ordering App! We're dedicated to making your food ordering experience effortless and
+        enjoyable.
+        Discover diverse cuisines from top local restaurants, carefully curated for quality and taste. Our user-friendly
+        interface ensures a seamless journey from selection to delivery. We're here 24/7 to ensure your satisfaction and
+        welcome your feedback to continuously enhance your experience. Join us and indulge in a world of flavors with
+        AK-Food Ordering App!
+    </div>
+</section>
